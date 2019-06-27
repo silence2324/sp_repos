bce4d821f7f8f162930a7557a8ff4a42c8235f6d
silence2324 <yangn_2324@163.com>
2019-06-27 17:18:00 +0800

    add  @Acc

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/User.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/User.java
@@ -3,10 +3,12 @@ package com.tedu.sp01.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Accessors(chain = true)
 public class User {
 	private Integer id;
 	private String username;
